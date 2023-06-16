4fb233ee66eb3f49c31ff31ebc51384e8e5e0c1e
ilystelo <ilyasti79@gmail.com>
2023-06-16 08:07:43 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
